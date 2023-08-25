5c6c5b8ad64193e961c468a2b5a9ee252b865249
alfrfan <irfaanroslii@gmail.com>
2023-08-25 22:59:39 +0800

    sambung

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """add x and y and return result"""
+    return x+y
+
+def substract(x,y):
+    return y-x
+    
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        result = calc.add(5,6)
+        self.assertEqual(result,11)
+    
+    def test_substract_numbers(self):
+        result=calc.substract(10,15)
+        self.assertEqual(result,5)
